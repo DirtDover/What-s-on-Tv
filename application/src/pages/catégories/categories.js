9acfb6a9a9615c3ae8ee2015f3b19edb7b737198
Benjamin Bureau <benjaminmbureau@gmail.com>
2023-06-22 16:13:58 +0200

    Card catégories

--- application/src/pages/catégories/categories.js
+++ application/src/pages/catégories/categories.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../catégories/categories.css'
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import Banner from '../../components/Banner'
+import Banner from '../../components/Banner';
+import Card from '../../components/Card';
 
-const categories = () => {
+const Categories = () => {
+
+    const auth = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjkxMjMzZmI0NTk5N2ExZTIxOTBmMzcwYTE2YTMwMiIsInN1YiI6IjY0OTAxNTJmMmY4ZDA5MDBlMzg2ODIzMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Z_pt-qopvTk-L1prSbBKzlGjyL8Gyq78YDMeASXUKvU'
+        }
+      };
+      
+      fetch('https://api.themoviedb.org/3/authentication', auth)
+        .then(response => response.json())
+        .catch(err => console.error(err));
+
+        const [tag, setTag] = useState([]);
+        
+        useEffect(()=>{
+          fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', auth)
+          .then(response => response.json())
+          .then(data => setTag(data.genres))
+          .then(console.log(tag))
+          .catch(err => console.error(err));
+        },[])
+
     return (
+
+        
+
         <div className='categories_container'>
             <Header />
             <Banner />
+            <main className='main_container_categories'>
+            <div className="categorie_container">
+                {tag.map((tags)=> (
+                       <Card key={tags.id} id={tags.id} title={tags.name}/>
+                    ))}
+            </div>        
+            </main>
             <Footer />
         </div>
     );
 };
 
-export default categories;
+export default Categories;
